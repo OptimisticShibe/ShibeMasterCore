22c356630fb47649937acef1356e1ea3d06a7735
OptimisticShibe <britishdragons123@gmail.com>
2017-08-15 10:04:12 -0500

    First commit. Rudimentary bot working

---
+++ ShibesBotCore/Modules/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace ShibesBotCore.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task TestCommand(string repeat)
+        {
+            await Context.Channel.SendMessageAsync(repeat);
+        }
+    }
+}
---
+++ ShibesBotCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.IO;
+using Discord.Commands;
+
+namespace ShibesBotCore
+{
+    public class Program : ModuleBase<SocketCommandContext>
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler.CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            string token = File.ReadLines(@"token.txt").Take(1).First();
+            /*var tokenArray = File.ReadAllLines("token.txt");
+            string token = tokenArray[0];*/
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler.CommandHandler();
+
+            await _handler.InitializeAsync(_client);
+
+            await Task.Delay(-1);
+        }
+    }
+}
